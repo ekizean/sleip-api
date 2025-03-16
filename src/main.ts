3b74422aeef74fa8629aa07f7fbda39219b0a53d
ekizean <simon@agriavetguide.com>
2025-03-16 11:43:05 +0100

    Enable versioning of api

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RolesGuard } from './common/guards/roles.guard';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const reflector = app.get(Reflector);
   app.useGlobalGuards(new RolesGuard(reflector));
   app.setGlobalPrefix('/api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
